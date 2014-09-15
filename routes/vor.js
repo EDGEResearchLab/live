4b5820efaa2fb098c3a979fff2f3396c7c327fb1
Matt Rasband <matt.rasband@gmail.com>
2014-09-15 19:56:05 -0400

    moved gps ops to a lib file, it may grow

--- routes/vor.js
+++ routes/vor.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var gps = require('../lib/gps');
 
 // Socket.IO namespace
 var namespace = null;
@@ -27,7 +28,7 @@ function handleNewPoint(point) {
     };
     getVors(function(docs) {
         for(var i = 0; i < docs.length; i++) {
-            docs[i]['distance'] = gpsDistanceTo(point.latitude, point.longitude, docs[i].latitude, docs[i].longitude);
+            docs[i]['distance'] = gps.distanceBetween(point.latitude, point.longitude, docs[i].latitude, docs[i].longitude);
         }
         docs.sort(function(a, b) {
             if (a.distance < b.distance) return -1;
@@ -57,27 +58,6 @@ function getVors(callback) {
         .then(callback)
         .catch(console.err);
 }
-
-var gpsDistanceTo = function(lat1, lon1, lat2, lon2) {
-    var nmiRadius = 3443.89849; // radius in nautical miles
-
-    var rads = [lat1, lon1, lat2, lon2].map(Math.radians);
-    var rlat1 = rads[0];
-    var rlon1 = rads[1];
-    var rlat2 = rads[2];
-    var rlon2 = rads[3];
-
-    var dLon = rlon2 - rlon1;
-    var dLat = rlat2 - rlat1;
-
-    var a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.pow(Math.sin(dLon / 2), 2);
-    return 2 * nmiRadius * Math.asin(Math.sqrt(a));
-};
-
-// Converts from degrees to radians.
-Math.radians = function(degrees) {
-  return degrees * Math.PI / 180;
-};
 
 module.exports = {
     router: router,
