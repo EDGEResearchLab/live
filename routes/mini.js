8987ec25b3e995850e0995a22fb16609234c4455
Matt Rasband <matt.rasband@gmail.com>
2014-09-17 11:55:54 -0400

    added dbo support for getting the latest flight, modified the mini.js route to use a pipeline to get the latest points on this flight

--- routes/mini.js
+++ routes/mini.js
@@ -6,28 +6,71 @@ var router = express.Router();
  * trackables and their latest position/altitude/speed
  */
 router.get('/', function(req, res) {
-    // TODO: Get Latest of All Points from the DB and build the reply
-    dbo.db.collection('flights')
-        .find()
-        .sort({begin: -1})
-        .limit(1)
-        .toArray(function(err, docs) {
-            dbo.db.collection('trackers')
-                .find({time: {$gt: docs[0].begin, $lt: docs[0].end}})
-                .sort({time: -1})
-                .toArray(function(err, docs) {
+    var myDbo = dbo;
+    dbo.getLatestFlight()
+        .then(function(doc) {
+            console.log('Latest Flight: ' + JSON.stringify(doc));
+            var aggregation = [
+                {
+                    // Find all in the latest flight (based on time)
+                    $match: {
+                        time: {
+                            $gte: doc.begin, 
+                            $lte: doc.end
+                        }
+                    }
+                },
+                {
+                    // Sort based on time, descending
+                    $sort: {
+                        time: -1
+                    }
+                },
+                {
+                    // Create a document with what we care about.
+                    $group: {
+                        _id: '$edgeId', 
+                        time: {
+                            $first: '$time'
+                        }, 
+                        latitude: {
+                            $first: '$latitude'
+                        },
+                        longitude: {
+                            $first: '$longitude'
+                        },
+                        altitude: {
+                            $first: '$altitude'
+                        },
+                        speed: {
+                            $first: '$speed'
+                        }
+                    }
+                }
+            ];
+            myDbo.aggregateTracks(aggregation)
+                .then(function(docs) {
+                    console.log('Found Docs: ' + docs);
+
                     var dataString = '';
                     for (var i = 0; i < docs.length; i++) {
                         var thisDoc = docs[i];
-                        dataString += 'Id:' + thisDoc.edgeId + '<br>';
+                        dataString += 'Id:' + thisDoc._id + '<br>';
                         dataString += 'Lat:' + thisDoc.latitude + '<br>';
                         dataString += 'Lon:' + thisDoc.longitude + '<br>';
                         dataString += 'Alt:' + thisDoc.altitude + '<br>';
                         dataString += 'Spd:' + thisDoc.speed + '<br>';
-                        dataString += '<hr/>';
+                        dataString += '<hr/><br/>';
                     }
+
                     res.status(200).send(dataString);
+                })
+                .catch(function(err) {
+                    res.status(500).send(err);  
                 });
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
         });
 });
 
